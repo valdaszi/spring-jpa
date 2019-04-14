69ba9b94caa305186114d64f937aaa5d3a976261
Valdas <valdas@e-servisas.lt>
2019-04-14 15:54:26 +0300

    first commit

---
+++ src/main/java/lt/bta/java2/sprngjpa/SprngJpaApplication.java
@@ -0,0 +1,69 @@
+package lt.bta.java2.sprngjpa;
+
+import lt.bta.java2.sprngjpa.entities.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SprngJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngJpaApplication.class, args);
+	}
+
+}
+
+@RepositoryRestResource(path = "product")
+interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+	List<Product> findByNameContains(String name);
+}
+
+
+@RestController
+@RequestMapping("/api/product")
+class ProductApi {
+
+	final private ProductRepository productRepository;
+
+	ProductApi(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> get(@PathVariable int id) {
+		return productRepository.findById(id).map(e -> ResponseEntity.ok(e)).orElseGet((() -> ResponseEntity.notFound().build()));
+	}
+
+	@GetMapping("/page")
+	public Page<Product> get(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sort) {
+		return productRepository.findAll(PageRequest.of(page, size, Sort.by(sort)));
+	}
+
+	@PostMapping
+	public Product add(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable int id) {
+		productRepository.deleteById(id);
+	}
+
+	@GetMapping("/find/name-like")
+	public List<Product> findByNameContains(@RequestParam String s) {
+		return productRepository.findByNameContains(s);
+	}
+}
+
+
